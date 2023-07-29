c6156a25825b6f3701871a4f632fb3cd96d01967
suparth <suparthnarayanghimire2014@gmail.com>
2023-07-30 00:46:35 +0545

    feat: chat ui

--- src/components/experience/design/controls/visitor/ChatControl.template.tsx
+++ src/components/experience/design/controls/visitor/ChatControl.template.tsx
@@ -1,10 +1,35 @@
+import { Modal, rem } from "@mantine/core";
 import { IconMessageCircle } from "@tabler/icons-react";
+import { useState } from "react";
+import { MARGIN } from "../../UI/ExperienceUI";
 import ExperienceIconButton from "../../common/IconButton";
-
+import ChatPanel from "../../panels/ChatPanel.template";
 const Chat = () => {
+	const [showChat, setShowChat] = useState(false);
 	return (
 		<>
-			<ExperienceIconButton>
+			<Modal
+				opened={showChat}
+				onClose={() => setShowChat(false)}
+				withCloseButton={false}
+				size={rem(350)}
+				overlayProps={{
+					opacity: 0,
+					blur: 0,
+				}}
+				radius={20}
+				padding={rem(24)}
+				styles={{
+					content: {
+						bottom: rem(100),
+						position: "absolute",
+						right: MARGIN,
+					},
+				}}
+			>
+				<ChatPanel />
+			</Modal>
+			<ExperienceIconButton onClick={() => setShowChat(true)}>
 				<IconMessageCircle />
 			</ExperienceIconButton>
 		</>
--- src/components/experience/design/panels/ChatPanel.template.tsx
+++ src/components/experience/design/panels/ChatPanel.template.tsx
@@ -1,5 +1,84 @@
+import { Box, ScrollArea, Text } from "@mantine/core";
+import { useEffect, useRef } from "react";
+
 const ChatPanel = () => {
-	return <div>ChatPanel</div>;
+	const chatPanelRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (!chatPanelRef.current) return;
+		chatPanelRef.current.scrollTo({
+			top: chatPanelRef.current.scrollHeight,
+			behavior: "smooth",
+		});
+	}, [chatPanelRef]);
+
+	return (
+		<div className="h-[344px] w-[302px]">
+			<div className="grid w-full gap-[12px]">
+				<ScrollArea
+					className="h-[340px]"
+					offsetScrollbars
+					viewportRef={chatPanelRef}
+				>
+					<div className="text-center">29 May 2023</div>
+					<ul className="p-0 grid gap-[12px]">
+						<li className="w-full flex justify-start">
+							<div className="w-full">
+								<div className="flex gap-2">
+									<Text weight={600}>david0212</Text>
+									<Text color="gray.5">12:59</Text>
+								</div>
+								<Text>Hello everyone!</Text>
+							</div>
+						</li>
+						<li className="w-full flex justify-start">
+							<div className="w-full">
+								<div className="flex gap-2">
+									<Text weight={600}>erin89</Text>
+									<Text color="gray.5">13:03</Text>
+								</div>
+								<Text>Hi.</Text>
+							</div>
+						</li>
+						<li className="w-full flex justify-start">
+							<div className="w-full">
+								<div className="flex gap-2">
+									<Text weight={600}>jeff0921</Text>
+									<Text color="gray.5">13:11</Text>
+								</div>
+								<Text>Welcome!</Text>
+							</div>
+						</li>
+						<li className="w-full flex justify-start">
+							<div className="w-full">
+								<div className="flex gap-2">
+									<Text weight={600}>jeff0921</Text>
+									<Text color="gray.5">13:11</Text>
+								</div>
+								<Text>Welcome!</Text>
+							</div>
+						</li>
+						<li className="w-full flex justify-end align-items-end">
+							<Box
+								className="p-[10px] rounded-[16px_16px_0px_16px]"
+								bg="blue.1"
+							>
+								<Text className="text-white">Welcome David!</Text>
+							</Box>
+						</li>
+						<li className="w-full flex justify-end align-items-end">
+							<Box
+								className="p-[10px] rounded-[16px_16px_0px_16px]"
+								bg="blue.1"
+							>
+								<Text className="text-white">Let’s meet by the Dior shop.</Text>
+							</Box>
+						</li>
+					</ul>
+				</ScrollArea>
+			</div>
+		</div>
+	);
 };
 
 export default ChatPanel;
