7f1720469c62fd52b5a0e7062eb6f78a7947c0ae
suparth <suparthnarayanghimire2014@gmail.com>
2023-08-03 12:35:35 +0545

    feat: asset sale

--- src/components/experience/design/controls/creator/shop/cart/CartControl.template.tsx
+++ src/components/experience/design/controls/creator/shop/cart/CartControl.template.tsx
@@ -1,10 +1,15 @@
 import ExperienceIconButton from "@/components/experience/design/common/IconButton";
+import { useDisclosure } from "@mantine/hooks";
 import { IconShoppingCart } from "@tabler/icons-react";
+import AssetSale from "./AssetSale.template";
 
 const Cart = () => {
+	const [opened, { open, close }] = useDisclosure(false);
 	return (
 		<>
-			<ExperienceIconButton color="blue.1">
+			<AssetSale opened={opened} onClose={close} />
+
+			<ExperienceIconButton color="blue.1" onClick={open}>
 				<IconShoppingCart />
 			</ExperienceIconButton>
 		</>
