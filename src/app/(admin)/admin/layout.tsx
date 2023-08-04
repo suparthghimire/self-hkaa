409dc9477349f8408d80fbe8a2912d47ed8387f9
suparth <suparthnarayanghimire2014@gmail.com>
2023-08-04 11:23:22 +0545

    refactor: remove single place signin persistence component with always signin

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -2,54 +2,64 @@
 import "@/app/globals.css";
 import AdminAuth from "@/components/auth/AdminAuth.template";
 import MainLayout from "@/components/layout/main/MainLayout.template";
-import { GetMyData } from "@/lib/api/api";
-import useCheckAuth from "@/lib/hooks/useCheckAuth";
-import { useAuth } from "@/lib/providers/Auth/AuthProvider";
-import { useMutation } from "@tanstack/react-query";
-import React, { PropsWithChildren, useEffect } from "react";
+import React, { PropsWithChildren } from "react";
+
+// const AdminPageLayout: React.FC<PropsWithChildren> = (props) => {
+// 	const { isAuth, accessToken } = useCheckAuth();
+
+// 	const { auth, setAuth } = useAuth();
+
+// 	const myInfo = useMutation({
+// 		mutationFn: (token: string) => GetMyData(token),
+// 	});
+
+// 	useEffect(() => {
+// 		console.log("ACCESS TOKEN", accessToken);
+// 		if (accessToken) myInfo.mutate(accessToken);
+// 	}, [accessToken]);
+
+// 	useEffect(() => {
+// 		if (!myInfo.data) return;
+// 		else if (!auth.status) return;
+// 		console.log(myInfo);
+// 		setAuth({
+// 			...auth,
+// 			user: {
+// 				...auth.user,
+// 				primary: {
+// 					...auth.user.primary,
+// 					...myInfo.data,
+// 				},
+// 			},
+// 		});
+// 	}, []);
+
+// 	if (isAuth) return <MainLayout userType="admin">{props.children}</MainLayout>;
+
+// 	if (myInfo.isLoading) return <>Loading My Info</>;
+
+// 	if (!isAuth || myInfo.isError)
+// 		return (
+// 			<AdminAuth>
+// 				<MainLayout userType="admin">{props.children}</MainLayout>;
+// 			</AdminAuth>
+// 		);
+
+// 	return <></>;
+// };
+
+/**
+ *
+ * @TODO
+ * Implement Single Place Sign On
+ */
 
 const AdminPageLayout: React.FC<PropsWithChildren> = (props) => {
-	const { isAuth, accessToken } = useCheckAuth();
-
-	const { auth, setAuth } = useAuth();
-
-	const myInfo = useMutation({
-		mutationFn: (token: string) => GetMyData(token),
-	});
-
-	useEffect(() => {
-		console.log("ACCESS TOKEN", accessToken);
-		if (accessToken) myInfo.mutate(accessToken);
-	}, [accessToken]);
-
-	useEffect(() => {
-		if (!myInfo.data) return;
-		else if (!auth.status) return;
-		console.log(myInfo);
-		setAuth({
-			...auth,
-			user: {
-				...auth.user,
-				primary: {
-					...auth.user.primary,
-					...myInfo.data,
-				},
-			},
-		});
-	}, []);
-
-	if (isAuth) return <MainLayout userType="admin">{props.children}</MainLayout>;
-
-	if (myInfo.isLoading) return <>Loading My Info</>;
-
-	if (!isAuth || myInfo.isError)
-		return (
-			<AdminAuth>
-				<MainLayout userType="admin">{props.children}</MainLayout>;
-			</AdminAuth>
-		);
-
-	return <></>;
+	return (
+		<AdminAuth>
+			<MainLayout userType="admin">{props.children}</MainLayout>;
+		</AdminAuth>
+	);
 };
 
 export default AdminPageLayout;
